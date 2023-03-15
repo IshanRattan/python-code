a572afabfdb9e68ff0a88a6713ba57b6e35901dc
irattan <git.demo12@gmail.com>
2023-03-14 22:49:30 -0700

    added caching for faster preview load time

--- ml/app_core/utils/helper.py
+++ ml/app_core/utils/helper.py
@@ -6,16 +6,11 @@ import streamlit as st
 def _createDataFrame(_fileUpload : bytes) -> pd.DataFrame:
     return pd.read_csv(_fileUpload)
 
-def _dataPreview(dataFrame: pd.DataFrame, rows: int = 5):
+@st.cache_data
+def _dataPreview(dataframe: pd.DataFrame, rows: int = 10):
     try:
-        return dataFrame.head(rows)
+        return dataframe.head(rows)
     except:
-        return dataFrame.head()
+        return dataframe.head()
 
-def _dropColumns(dataFrame : pd.DataFrame, colNames : list) -> pd.DataFrame:
-    # User Input
-    return dataFrame.drop(colNames, axis = 1)
 
-def _removeCols(dataFrame, selectedCols):
-    if selectedCols is not None and dataFrame is not None:
-        st.session_state.dataFrame = _dropColumns(dataFrame, selectedCols)
