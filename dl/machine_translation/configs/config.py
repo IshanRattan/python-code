e030228dd1330924a97cc2e4d81a707fde5b9c1b
irattan <git.demo12@gmail.com>
2023-06-17 21:23:13 -0700

    updated config.py

--- dl/machine_translation/configs/config.py
+++ dl/machine_translation/configs/config.py
@@ -5,13 +5,13 @@ MAX_LENGTH = 20
 BATCH_SIZE = 64
 BUFFER_SIZE = 20000
 
-path_en_input = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/transformer/data/fr-en/europarl-v7.fr-en.en'
-path_fr_input = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/transformer/data/fr-en/europarl-v7.fr-en.fr'
+path_en_input = 'europarl-v7.fr-en.en'
+path_fr_input = 'europarl-v7.fr-en.fr'
 
-path_en_nonbreak = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/transformer/data/fr-en/P85-Non-Breaking-Prefix.en'
-path_fr_nonbreak = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/transformer/data/fr-en/P85-Non-Breaking-Prefix.fr'
+path_en_nonbreak = 'P85-Non-Breaking-Prefix.en'
+path_fr_nonbreak = 'P85-Non-Breaking-Prefix.fr'
 
 
 # model
-checkpoint_path = "/Users/ishanrattan/Desktop/Study/text/machinetranslation/transformer/ckpt/"
+checkpoint_path = "./transformer/ckpt/"
 
