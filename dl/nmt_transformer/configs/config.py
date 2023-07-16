c84823b49767612270a4bfea8a287aad4e7621af
irattan <git.demo12@gmail.com>
2023-07-15 17:56:14 -0700

    updated config.py

--- dl/nmt_transformer/configs/config.py
+++ dl/nmt_transformer/configs/config.py
@@ -7,13 +7,13 @@ BUFFER_SIZE = 20000
 
 TRAINING_DATA_POINTS = 50000 # -1 to use complete training data
 
-path_en_input = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/old/data/en.txt' #'/Users/ishanrattan/Desktop/Study/text/machinetranslation/nmt_transformer/data/fr-en/europarl-v7.fr-en.en'
-path_fr_input = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/old/data/fr.txt' #'/Users/ishanrattan/Desktop/Study/text/machinetranslation/nmt_transformer/data/fr-en/europarl-v7.fr-en.fr'
+path_en_input = 'en.txt'
+path_fr_input = 'fr.txt'
 
-path_en_nonbreak = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/nmt_transformer/data/fr-en/P85-Non-Breaking-Prefix.en'
-path_fr_nonbreak = '/Users/ishanrattan/Desktop/Study/text/machinetranslation/nmt_transformer/data/fr-en/P85-Non-Breaking-Prefix.fr'
+path_en_nonbreak = 'P85-Non-Breaking-Prefix.en'
+path_fr_nonbreak = 'P85-Non-Breaking-Prefix.fr'
 
 
 # model
-checkpoint_path = "/Users/ishanrattan/Desktop/Study/text/machinetranslation/nmt_transformer/ckpt/"
+checkpoint_path = "nmt_transformer/ckpt/"
 
