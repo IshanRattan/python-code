9729a99d8d51a17846535573b3426cdafd4a8d0d
IshanRattan <ishanrattan12@gmail.com>
2023-08-14 23:02:52 -0700

    added img transformations function

--- dl/image/classification/train.py
+++ dl/image/classification/train.py
@@ -1,4 +1,26 @@
 
+from torchvision import transforms, datasets
+import os
+
+
+def transformations():
+    # Image transformations
+    data_transformations = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    return data_transformations
+
 
 def train_model():
     pass
