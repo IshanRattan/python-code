e06fed317308a3b02cddabf3b4e9f4e816cad54b
IshanRattan <ishanrattan12@gmail.com>
2023-08-19 13:55:53 -0700

    added config.py

---
+++ dl/text/nmt_transformer/configs/config.py
@@ -0,0 +1,19 @@
+
+# data processing
+MAX_LENGTH = 20
+
+BATCH_SIZE = 15
+BUFFER_SIZE = 20000
+
+TRAINING_DATA_POINTS = 50000 # -1 to use complete training data
+
+path_en_input = 'en.txt'
+path_fr_input = 'fr.txt'
+
+path_en_nonbreak = 'P85-Non-Breaking-Prefix.en'
+path_fr_nonbreak = 'P85-Non-Breaking-Prefix.fr'
+
+
+# model
+checkpoint_path = "nmt_transformer/ckpt/"
+
