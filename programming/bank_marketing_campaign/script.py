5e9b626c7fd3c775dcbee24873a8cf2b76ce2e68
IshanRattan <ishanrattan12@gmail.com>
2023-12-01 16:49:12 +0530

    added code to generate campaign.csv

--- programming/bank_marketing_campaign/script.py
+++ programming/bank_marketing_campaign/script.py
@@ -16,3 +16,13 @@ client['credit_default'] = client['credit_default'].map({'false': False, 'true':
 client.to_csv('client.csv')
 
 
+campaign = infile[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome', 'month', 'day']]
+campaign['previous_outcome'] = campaign['previous_outcome'].str.replace('nonexistent', 'failure')
+campaign['previous_outcome'] = campaign['previous_outcome'].map({'failure':False, 'success':True})
+campaign['campaign_outcome'] = campaign['campaign_outcome'].map({'no':False, 'yes':True})
+campaign['last_contact_date'] = '2022-' + campaign['month'].astype('str') + '-' + campaign['day'].astype('str')
+campaign['last_contact_date'] = pd.to_datetime(campaign['last_contact_date'], format="%Y-%b-%d")
+campaign.drop(['month', 'day'], axis=1, inplace=True)
+campaign.to_csv('campaign.csv')
+
+
