6a4c3a52e068a75e298aee6941a22ea9cf6912be
IshanRattan <ishanrattan12@gmail.com>
2023-12-01 16:48:50 +0530

    added code to generate client.csv

--- programming/bank_marketing_campaign/script.py
+++ programming/bank_marketing_campaign/script.py
@@ -1,5 +1,18 @@
+
 import pandas as pd
 import numpy as np
 
 infile = pd.read_csv("bank_marketing.csv")
 infile.head()
+
+
+client = infile[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']]
+client['job'] = client['job'].str.replace('.', '_')
+client['education'] = client['education'].str.replace('.', '_').replace("unknown", np.NaN)
+client['mortgage'] = client['mortgage'].replace('no', 'false').replace('yes', 'true').replace('unknown', 'false')
+client['mortgage'] = client['mortgage'].map({'false': False, 'true': True})
+client['credit_default'] = client['credit_default'].replace('no', 'false').replace('yes', 'true').replace('unknown', 'false')
+client['credit_default'] = client['credit_default'].map({'false': False, 'true': True})
+client.to_csv('client.csv')
+
+
