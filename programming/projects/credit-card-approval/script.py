02a65dbf6679cfe8091559dfb7bd3ca725f86884
IshanRattan <ishanrattan12@gmail.com>
2023-12-26 09:37:52 +0530

    updated script.py

--- programming/projects/credit-card-approval/script.py
+++ programming/projects/credit-card-approval/script.py
@@ -1,2 +1,67 @@
 
 # Code to predict credit card approval
+
+import pandas as pd
+
+# Load dataset
+cc_apps = pd.read_csv("/Users/ishanrattan/Desktop/Study/github/python-code/programming/projects/credit-card-approval/datasets/cc_approvals.data",
+                      header=None)
+
+# Inspect data
+print(cc_apps.head())
+
+
+# Print summary statistics
+cc_apps_description = cc_apps.describe()
+print(cc_apps_description)
+
+print('\n')
+
+# Print DataFrame information
+cc_apps_info = cc_apps.info()
+print(cc_apps_info)
+
+print('\n')
+
+# Inspect missing values in the dataset
+print(cc_apps.tail(17))
+
+
+# Import train_test_split
+from sklearn.model_selection import train_test_split
+
+# Drop the features 11 and 13
+cc_apps = cc_apps.drop(cc_apps.columns[[11, 13]], axis=1)
+
+# Split into train and test sets
+cc_apps_train, cc_apps_test = train_test_split(cc_apps, test_size=0.33, random_state=42)
+
+
+# Import numpy
+import numpy as np
+
+# Replace the '?'s with NaN in the train and test sets
+cc_apps_train = cc_apps_train.replace('?', np.nan)
+cc_apps_test = cc_apps_test.replace('?', np.nan)
+
+
+# Impute the missing values with mean imputation
+cc_apps_train.fillna(cc_apps_train.mean(), inplace=True)
+cc_apps_test.fillna(cc_apps_test.mean(), inplace=True)
+
+# Count the number of NaNs in the datasets and print the counts to verify
+print(cc_apps_train.isnull().sum())
+print(cc_apps_test.isnull().sum())
+
+
+# Iterate over each column of cc_apps_train
+for col in cc_apps_train:
+    # Check if the column is of object type
+    if cc_apps_train[col].dtypes == 'object':
+        # Impute with the most frequent value
+        cc_apps_train = cc_apps_train.fillna(cc_apps_train[col].value_counts().index[0])
+        cc_apps_test = cc_apps_test.fillna(cc_apps_test[col].value_counts().index[0])
+
+# Count the number of NaNs in the dataset and print the counts to verify
+print(cc_apps_train.isnull().sum())
+print(cc_apps_test.isnull().sum())
