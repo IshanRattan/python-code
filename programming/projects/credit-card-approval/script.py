ce70f07bacc46c5c40b8c68f3858abc689809bc7
IshanRattan <ishanrattan12@gmail.com>
2023-12-28 09:40:10 +0530

    updated script.py

--- programming/projects/credit-card-approval/script.py
+++ programming/projects/credit-card-approval/script.py
@@ -65,3 +65,48 @@ for col in cc_apps_train:
 # Count the number of NaNs in the dataset and print the counts to verify
 print(cc_apps_train.isnull().sum())
 print(cc_apps_test.isnull().sum())
+
+
+# Convert the categorical features in the train and test sets independently
+cc_apps_train = pd.get_dummies(cc_apps_train)
+cc_apps_test = pd.get_dummies(cc_apps_test)
+
+# Reindex the columns of the test set aligning with the train set
+cc_apps_test = cc_apps_test.reindex(columns=cc_apps_train.columns,
+                                    fill_value=0)
+
+
+# Import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Segregate features and labels into separate variables
+X_train, y_train = cc_apps_train.iloc[:, :-1].values, cc_apps_train.iloc[:, [-1]].values
+X_test, y_test = cc_apps_test.iloc[:, :-1].values, cc_apps_test.iloc[:, [-1]].values
+
+# Instantiate MinMaxScaler and use it to rescale X_train and X_test
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX_train = scaler.fit_transform(X_train)
+rescaledX_test = scaler.transform(X_test)
+
+
+# Import LogisticRegression
+from sklearn.linear_model import LogisticRegression
+
+# Instantiate a LogisticRegression classifier with default parameter values
+logreg = LogisticRegression()
+
+# Fit logreg to the train set
+logreg.fit(rescaledX_train, y_train)
+
+
+# Import confusion_matrix
+from sklearn.metrics import confusion_matrix
+
+# Use logreg to predict instances from the test set and store it
+y_pred = logreg.predict(rescaledX_test)
+
+# Get the accuracy score of logreg model and print it
+print("Accuracy of logistic regression classifier: ", logreg.score(rescaledX_test, y_test))
+
+# Print the confusion matrix of the logreg model
+print(confusion_matrix(y_test, y_pred))
