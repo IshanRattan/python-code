d5b35f244c787a50329c668c208a4ca227fe9fcb
IshanRattan <ishanrattan12@gmail.com>
2024-03-03 17:01:03 -0800

    added demo.py

---
+++ programming/fastapi-fundamentals/demo.py
@@ -0,0 +1,34 @@
+
+from typing import Optional, List
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
+
+
+
+app = FastAPI()
+
+db = [{'id': 1, 'size': 's', 'fuel': 'gasoline', 'doors': 4, 'transmission': 'auto'},
+      {'id': 2, 'size': 's', 'fuel': 'gasoline', 'doors': 3, 'transmission': 'auto'},
+      {'id': 3, 'size': 's', 'fuel': 'gasoline', 'doors': 2, 'transmission': 'auto'}]
+
+
+@app.get('/')
+def welcome():
+    """Return a welcome message."""
+    return {'message': 'Welcome to the car sharing service!'}
+
+@app.get('/date')
+def date():
+    return {'date': datetime.now()}
+
+@app.get('/api/cars')
+def get_cars(size: Optional[str]=None, doors: Optional[int]=None) -> List:
+    result = db
+    if size:
+        result = [car for car in result if car['size'] == size]
+    if doors and doors <=5:
+        result = [car for car in result if car['doors'] >= doors]
+    else:
+        raise HTTPException(status_code=404, detail='No car with doors greater than 5!')
+
+    return result
