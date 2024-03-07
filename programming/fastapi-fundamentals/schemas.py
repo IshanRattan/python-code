d6c49224ef4ba4336a5acd9d7fc7ea663e105b61
IshanRattan <ishanrattan12@gmail.com>
2024-03-06 23:26:33 -0800

    added load_db & save_db functions

--- programming/fastapi-fundamentals/schemas.py
+++ programming/fastapi-fundamentals/schemas.py
@@ -1,6 +1,8 @@
 
 from pydantic import BaseModel
 
+import json
+
 # data schema
 class Car(BaseModel):
     id: int
@@ -8,3 +10,13 @@ class Car(BaseModel):
     fuel: str | None = 'electric'
     doors: int
     transmission: str | None = 'auto'
+
+
+def load_db():
+    with open('cars.json', 'r') as f:
+        return [Car.parse_obj(obj) for obj in json.load(f)]
+
+
+def save_db(cars: list[Car]):
+    with open('cars.json', 'r') as f:
+        json.dump([car.dict() for car in cars], f, indent=4)
