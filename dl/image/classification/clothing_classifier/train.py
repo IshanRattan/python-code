498579aa4781e0ed6b28868a836b620f05779193
IshanRattan <ishanrattan12@gmail.com>
2024-03-13 17:15:10 -0700

    added train.py

---
+++ dl/image/classification/clothing_classifier/train.py
@@ -0,0 +1,10 @@
+
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchmetrics import Accuracy, Precision, Recall
+
+
