c091b4b146ba8643e6091a9b010eacddd08c0813
IshanRattan <ishanrattan12@gmail.com>
2024-03-14 17:16:11 -0700

    updated train.py

--- dl/image/classification/clothing_classifier/train.py
+++ dl/image/classification/clothing_classifier/train.py
@@ -8,3 +8,11 @@ from torch.utils.data import Dataset, DataLoader
 from torchmetrics import Accuracy, Precision, Recall
 
 
+# Load datasets
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+train_data = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+test_data = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
+
+
