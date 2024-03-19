b95f8351b669f3e9592511009a5083b21cd30e5a
IshanRattan <ishanrattan12@gmail.com>
2024-03-18 18:42:27 -0700

    added comments

--- programming/consolidating_employee_data/script.py
+++ programming/consolidating_employee_data/script.py
@@ -1,4 +1,39 @@
 
+"""
+You just got hired as the first and only data practitioner at a small business experiencing exponential growth.
+The company needs more structured processes, guidelines, and standards.
+Your first mission is to structure the human resources data.
+The data is currently scattered across teams and files and comes in various formats: Excel files, CSVs, JSON files...
+
+Following is the data in the `datasets` folder:
+- __Office addresses__
+    - Saved in office_addresses.csv.
+    - If the value for office is `NaN`, then the employee is remote.
+- __Employee addresses__
+    - Saved on the first tab of `employee_information.xlsx`.
+- __Employee emergency contacts__
+    - Saved on the second tab of `employee_information.xlsx`; this tab is called `emergency_contacts`.
+    - However, this sheet was edited at some point, and ***the headers were removed***!
+    The HR manager let you know that they should be:
+        `employee_id`, `last_name`, `first_name`, `emergency_contact`, `emergency_contact_number`, and `relationship`.
+- __Employee roles, teams, and salaries__
+    - This information has been exported from the company's human resources management system into a JSON file titled
+    `employee_roles.json`. Here are the first few lines of that file:
+
+    {"A2R5H9":
+      {
+        "title": "CEO",
+        "monthly_salary": "$4500",
+        "team": "Leadership"
+      },
+     ...
+    }
+    
+"""
+
+
+
+
 import pandas as pd
 
 # df contains office addresses
