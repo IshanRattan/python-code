5f9879f5ed2fba9c8000bdb08ef7dfc3ce6ccb8d
IshanRattan <ishanrattan12@gmail.com>
2024-03-26 21:54:59 -0700

    added @task create_table

--- programming/airflow/dags/data_transformation_storage_pipeline.py
+++ programming/airflow/dags/data_transformation_storage_pipeline.py
@@ -27,4 +27,18 @@ def data_transformation_storage_pipeline():
         df = pd.read_csv('/root/airflow/datasets/car_data.csv')
         return df.to_json()
 
+    @task(task_id='create_table')
+    def create_table():
+        sqlite_operator = SqliteOperator(
+            sql_conn_id = 'my_sqlite_conn',
+            sql = """CREATE TABLE IF NOT EXISTS car_data (
+            id INTEGER PRIMARY KEY,
+            brand TEXT NOT NULL,
+            model TEXT NOT NULL,
+            body_style TEXT NOT NULL,
+            seat INTEGER NOT NULL,
+            price INTEGER NOT NULL);""")
+
+        sqlite_operator.execute(context=None)
+
 
