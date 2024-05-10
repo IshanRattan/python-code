2bd6a6c3eafe10c518930a0ce428e1e576228aee
IshanRattan <ishanrattan12@gmail.com>
2024-05-10 15:54:01 +0530

    updated config.py

--- dl/image/classification/cifar10/config.py
+++ dl/image/classification/cifar10/config.py
@@ -4,3 +4,6 @@ class_names = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 's
 data_path = './dataset/'
 batch_size = 64
 device = 'mps'
+
+########################
+learning_rate = .01
